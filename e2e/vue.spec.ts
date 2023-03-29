b929c44f217c5c318754e496e0ef8d8e92a6d5e3
Panoptic-QS Eialarasu <arasu@panoptic-qs.com>
2023-03-29 20:51:08 +0530

    updating latest code

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -2,8 +2,15 @@ import { test, expect } from '@playwright/test';
 
 // See here how to get started:
 // https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
-  //https://dev-next.auth.dev.amwell.systems/app/SMART/smarthealthit/practitioner/launch?visittype=pnp&iss=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&launch=WzAsImIyMThjZWU5LTAxOWQtNDdhNC1iMTYxLWU5N2MwZmQ2ZjczNiIsIjUxMWFiNDViLWEwMjAtNGMyNC05M2ZmLTkxOTM2NDgzY2M1NSIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0
-  await page.goto('https://dev-next.auth.dev.amwell.systems/app/SMART/smarthealthit/practitioner/launch?visittype=pnp&iss=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&launch=WzAsImIyMThjZWU5LTAxOWQtNDdhNC1iMTYxLWU5N2MwZmQ2ZjczNiIsIjUxMWFiNDViLWEwMjAtNGMyNC05M2ZmLTkxOTM2NDgzY2M1NSIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0');
-  //await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
-})
+test('verify patient in queue', async ({ page }) => {
+  await page.goto('https://launch.smarthealthit.org/?launch_url=https%3A%2F%2Fdev-next.auth.dev.amwell.systems%2Fapp%2FSMART%2Fsmarthealthit%2Fpractitioner%2Flaunch%3Fvisittype%3Dpnp&launch=WzAsImIyMThjZWU5LTAxOWQtNDdhNC1iMTYxLWU5N2MwZmQ2ZjczNiIsIjUxMWFiNDViLWEwMjAtNGMyNC05M2ZmLTkxOTM2NDgzY2M1NSIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0');
+  const page1Promise = page.waitForEvent('popup');
+  await page.getByRole('link', { name: 'Launch', exact: true }).click();
+  const page1 = await page1Promise;
+  //await page1.goto('https://dev-next.auth.dev.amwell.systems/app/SMART/smarthealthit/practitioner/launch?visittype=pnp&iss=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&launch=WzAsImIyMThjZWU5LTAxOWQtNDdhNC1iMTYxLWU5N2MwZmQ2ZjczNiIsIjUxMWFiNDViLWEwMjAtNGMyNC05M2ZmLTkxOTM2NDgzY2M1NSIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0');
+  await page1.waitForNavigation();
+  await page1.waitForTimeout(5000);
+  await expect(page1.locator("h3.left m-0")).toContainText("Next Patient");
+  await expect(page1.locator("h5.mb2 strong")).toContainText("Patients in Queue");
+  await expect(page1.locator("p.count m-0")).toContainText("1");
+});
--- playwright.config.ts
+++ playwright.config.ts
@@ -13,13 +13,13 @@ import { devices } from '@playwright/test'
 const config: PlaywrightTestConfig = {
   testDir: './e2e',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 15000
   },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
